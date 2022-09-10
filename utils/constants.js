3aef14e8323ce504a8e2dae3dcaef8451d5bff41
Nikolay Saitov <saitovkms@gmail.com>
2022-09-10 16:15:53 +0300

    dfdersgvc

--- utils/constants.js
+++ utils/constants.js
@@ -6,7 +6,7 @@ const {
 
 const secretKey = NODE_ENV === 'production' ? JWT_SECRET : 'secret-key';
 
-const addressMongoDB = NODE_ENV === 'production' ? DATABASE_URI : 'https://data.mongodb-api.com/app/data-ndkxm/endpoint/data/v1';
+const addressMongoDB = NODE_ENV === 'production' ? DATA_BASE : 'mongodb://localhost:27017/kino';
 
 module.exports = {
   addressMongoDB,
