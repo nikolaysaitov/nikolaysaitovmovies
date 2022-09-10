17781c8e9df679dbbce887fae383f37d03fe25d0
Nikolay Saitov <saitovkms@gmail.com>
2022-09-10 16:43:36 +0300

    hjdgh

--- utils/constants.js
+++ utils/constants.js
@@ -1,12 +1,12 @@
 const {
   NODE_ENV,
   JWT_SECRET,
-  DATA_BASE = 'mongodb://localhost:27017/kino',
+  DATA_BASE = 'mongodb+srv://nikolay:<password>@cluster0.n8rwhhk.mongodb.net/?retryWrites=true&w=majority',
 } = process.env;
 
 const secretKey = NODE_ENV === 'production' ? JWT_SECRET : 'secret-key';
 
-const addressMongoDB = NODE_ENV === 'production' ? DATA_BASE : 'mongodb://localhost:27017/kino';
+const addressMongoDB = NODE_ENV === 'production' ? DATA_BASE : 'mongodb+srv://nikolay:<password>@cluster0.n8rwhhk.mongodb.net/?retryWrites=true&w=majority';
 
 module.exports = {
   addressMongoDB,
